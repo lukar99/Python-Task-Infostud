565e675d1d24f38cd8f3b8112f1096318fd17784
luka99r <luka99.r@gmail.com>
2022-03-23 13:43:55 +0100

    Create get ad per city and employer api's

--- infostudapi/ApiApp/urls.py
+++ infostudapi/ApiApp/urls.py
@@ -3,5 +3,7 @@ from ApiApp import views
 
 
 urlpatterns=[
-    path('getads/',views.JobAdsGenericView.as_view(), name='getads')
+    path('getads/',views.JobAdsGenericView.as_view(), name='getads'),
+    path('getads/city/<str:pk>', views.JobAdsCityGenericView.as_view(), name='jobcity'),
+    path('getads/employer/<str:pk>/', views.JobAdsEmployerGenericView.as_view(), name='jobcity'),
 ]
--- infostudapi/ApiApp/views.py
+++ infostudapi/ApiApp/views.py
@@ -1,7 +1,7 @@
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.generics import GenericAPIView
 
-from rest_framework import mixins
+from rest_framework import mixins, generics, filters
 
 from ApiApp.models import JobAds
 from ApiApp.serializers import AdsSerializer
@@ -13,3 +13,18 @@ class JobAdsGenericView(mixins.ListModelMixin, mixins.CreateModelMixin, GenericA
     
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+class JobAdsCityGenericView(generics.ListAPIView):
+    serializer_class = AdsSerializer
+
+    def get_queryset(self):
+        city = self.kwargs['pk']
+        return JobAds.objects.filter(job_address__contains=city) # added __contains method to enlist ads with multiple job addresses
+
+class JobAdsEmployerGenericView(generics.ListAPIView):
+    serializer_class = AdsSerializer
+
+    def get_queryset(self):
+        employer = self.kwargs['pk']
+        return JobAds.objects.filter(employer=employer)
+        
